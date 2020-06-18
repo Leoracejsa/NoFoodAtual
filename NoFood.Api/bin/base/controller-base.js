1facaba871fd4ec2c68616df8621303e865b9e5a
Leonardo Jose da Silva Ahmed <leonardo.ahmed@unigranrio.br>
2020-06-18 10:13:49 -0300

    controller base

--- NoFood.Api/bin/base/controller-base.js
+++ NoFood.Api/bin/base/controller-base.js
@@ -1,26 +1,78 @@
 exports.post = async(repository, validationContract, req, res) => {
     try{
+        let data = req.body;        
+        if (!validationContract.isValid()){
+            res.status(400).send({message: 'Existem dados inválidos na sua requisição',
+            validation: validationContract.error()
+            }).end();
+            return;
+        }
+        let resultado = await repository.create(data);
+        res.status(201).send(resultado);
 
-        let data = req.body;
-        
-    }catch(error){
-
+    }catch(err){
+        console.log('Post com error, motivo: ', err);
+        res.status(500).send({message: 'Erro no processamento', error: err});
     }
 
 };
 
 exports.put = async(repository, validationContract, req, res) => {
-    
+    try{
+        let data = req.body;        
+        if (!validationContract.isValid()){
+            res.status(400).send({message: 'Existem dados inválidos na sua requisição',
+            validation: validationContract.error()
+            }).end();
+            return;
+        }
+
+        let resultado = await repository.update(req.params.id, data);
+        res.status(202).send(resultado);
+
+    }catch(err){
+        console.log('Put com error, motivo: ', err);
+        res.status(500).send({message: 'Erro no processamento', error: err});
+    }
+
 };
 
 exports.get = async(repository, req, res) => {
-    
+    try{
+        let data = await repository.getAll();
+        res.status(200).send(data);
+    }catch(err){
+        console.log('get com error, motivo: ', err);
+        res.status(500).send({message: 'Erro no processamento', error: err});
+    }
 };
 
 exports.getById = async(repository, req, res) => {
-    
+    try{
+        let id = req.params.id;
+        if(id){
+            let data = await repository.getById(id);
+            res.status(200).send(data);
+        }else{
+            res.status(400).send({message: 'O parâmetro Id precisa ser informado.' });
+        }
+    }catch(err){
+        console.log('getById com error, motivo: ', err);
+        res.status(500).send({message: 'Erro no processamento', error: err});
+    }
 };
 
 exports.delete = async(repository, req, res) => {
-    
+    try {
+        let id = req.params.id;
+            if(id){
+    let data = await repository.delete(id);
+    res.status(200).send({message: 'Registro excluído com sucesso'});
+            }else{
+                res.status(400).send({message: 'O parâmetro Id precisa ser informado.' });
+            }        
+    }catch(err) {
+        console.log('Get com error, motivo: ', err);
+        res.status(500).send({message: 'Erro no processamento', error: err});
+    }
 };
