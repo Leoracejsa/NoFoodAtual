350028063a3fcd027a8b1808741a7e10ac47e12b
Leonardo Jose da Silva Ahmed <leonardo.ahmed@unigranrio.br>
2020-06-19 16:04:24 -0300

    App

---
+++ NoFood.Ionic/src/providers/http/http.ts
@@ -0,0 +1,44 @@
+import { AlertProvider } from './../alert/alert';
+import { HttpResultModel } from './../../app/models/HttpResultModel';
+import { NetworkProvider } from './../network/network';
+import { SpinnerProvider } from './../spinner/spinner';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class HttpProvider {
+
+  constructor(
+    private http: HttpClient,
+    private spinnerSrv: SpinnerProvider,
+    private alertSrv: AlertProvider,
+    private networkSrv: NetworkProvider
+    ) {
+    
+  }
+
+public get(url: string): Promise<HttpResultModel> {
+    this.spinnerSrv.Show("Carregando os dados..."); 
+
+    return new Promise((resolve) => {
+      if (this.networkSrv.isOnline) {
+        
+        this.http.get(url).subscribe(_res => {
+          this.spinnerSrv.Hide();
+          resolve({success: true, data: _res, err: undefined});
+        }, err => {
+          this.spinnerSrv.Hide();
+          this.alertSrv.toast('Não foi possível consultar os dados, verifique sua conexão e tente novamente', 'bottom');
+          resolve({success: false, data: undefined, err: err});
+        });
+
+      }
+      else{
+        this.alertSrv.toast('Você está offline, e infelizmente não pode ser carregado os dados!', 'bottom');
+        resolve({success: true, data: [], err: undefined});
+      }
+    });
+
+  }
+
+}
